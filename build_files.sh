74b455de0e2ac4c7018d035f10a430e23f0d7a6c
JeremyJS <jsjeremy4@gmail.com>
2026-02-24 18:20:04 -0400

    Remove invalid --no-input argument from tailwind commands

--- build_files.sh
+++ build_files.sh
@@ -3,8 +3,8 @@ echo "Building project deployments..."
 python3 -m pip install -r requirements.txt --break-system-packages
 
 echo "Building Tailwind CSS..."
-python3 manage.py tailwind install --no-input
-python3 manage.py tailwind build --no-input
+python3 manage.py tailwind install
+python3 manage.py tailwind build
 
 echo "Running collectstatic..."
 python3 manage.py collectstatic --noinput --clear
