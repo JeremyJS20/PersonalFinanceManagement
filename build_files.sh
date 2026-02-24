9b7ccef838433889a97270de6195d94ee38c0ca8
JeremyJS <jsjeremy4@gmail.com>
2026-02-24 18:16:03 -0400

    Fix PEP 668 externally managed environment in build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Building project deployments..."
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt --break-system-packages
 echo "Running collectstatic..."
 python3 manage.py collectstatic --noinput --clear
